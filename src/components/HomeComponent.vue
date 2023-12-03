3d5c1846d20df1f796f6b0560e368e64b8ce10cf
juaneins_uio <ballesterosjg@gmail.com>
2023-12-03 11:11:03 -0500

    Usar slots para mostrar movimientos

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -1,12 +1,19 @@
 <script setup>
 import LayoutComponent from './LayoutComponent.vue';
 import HeaderComponent from './HeaderComponent.vue';
-
+import ResumeComponent from './Resume/ResumeComponent.vue';
+import MovementsComponent from './MovementsComponent.vue';
 </script> 
 <template>
     <LayoutComponent>
         <template #header>
             <HeaderComponent />
+        </template>
+        <template #resume>
+            <ResumeComponent />
+        </template>
+        <template #movements>
+            <MovementsComponent />
         </template>
     </LayoutComponent>
 </template>
