82503cd7c0dc50efc22c83aef28386e784b37845
juaneins_uio <ballesterosjg@gmail.com>
2023-12-03 10:00:19 -0500

    Init commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+//import HomeComponent from '@/components/HomeComponent.vue';
+//import SplashScreen from '@/components/SplashScreen.vue';
+import { defineAsyncComponent } from 'vue';
+
+const HomeAsyncComponent = defineAsyncComponent(() => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(import('./components/HomeComponent.vue'));
+    }, 2500);
+  });
+});
+</script>
+
+<template>
+  <HomeAsyncComponent />
+</template>
+
+<style>
+html,
+body,
+.app {
+  min-height: 100vh;
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+* {
+  --brand-green: #04b500;
+  --brand-blue: #0689b0;
+}
+</style>
